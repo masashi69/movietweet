d699b1505c54a57ba4952e4520fecc3d19b942bc
Masashi Suzuki <smasa619@gmail.com>
2020-05-17 13:32:57 +0900

    Modify posttw.py to use api file

--- posttw.py
+++ posttw.py
@@ -1,15 +1,9 @@
-import tweepy
+#!/usr/bin/env python3
+
+from tweetapi import TWauth
 import sys
-import configure
 
-Consumer_Key=configure.CK
-Consumer_Secret=configure.CS
-Access_Token=configure.AT
-Access_Secret=configure.AS
-
-auth = tweepy.OAuthHandler(Consumer_Key, Consumer_Secret)
-auth.set_access_token(Access_Token, Access_Secret)
-api = tweepy.API(auth)
+api = TWauth()
 
 # ツイート
 api.update_status(sys.argv[1])
