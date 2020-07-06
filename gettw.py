4eb22a094706af4407af6fa36f192554f340709b
Masashi Suzuki <smasa619@gmail.com>
2020-07-06 22:16:46 +0900

    Add pager

--- gettw.py
+++ gettw.py
@@ -3,11 +3,14 @@
 from tweetapi import TWauth
 import datetime
 
-for info in TWauth().user_timeline(count=200):
-    if 'tweet movieinfo' in info.source:
-        Posted = info.created_at + datetime.timedelta(hours=9)
-        Text = info.text[info.text.find('「')+1:info.text.find('」')]
-        print(Posted, Text)
-    else:
-        pass
+API = TWauth()
+
+for i in range(1, 5):
+    for info in API.user_timeline(count=200, page=i):
+        if 'tweet movieinfo' in info.source:
+            Posted = info.created_at + datetime.timedelta(hours=9)
+            Text = info.text[info.text.find('「')+1:info.text.find('」')]
+            print(Posted, Text)
+        else:
+            pass
 
