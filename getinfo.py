5636f0f948dd6a2a82de58b6f44891acaadf2cf4
Masashi Suzuki <smasa619@gmail.com>
2021-07-24 12:09:09 +0900

    Create movie infomation get function

---
+++ getinfo.py
@@ -0,0 +1,23 @@
+import requests
+import sys
+import json
+import configure
+import re
+
+KEY = configure.APIKEY
+url = 'http://www.omdbapi.com/'
+gettitle = '+'.join(sys.argv[1:])
+getinfo = ''.join([url, '?apikey=', KEY, '&t=', gettitle])
+
+def main():
+
+	movieinfo = requests.get(getinfo)
+	dataload = json.loads(movieinfo.text)
+
+	datadump = json.dumps(dataload, indent=0)
+	shapedata = re.sub('"|,', "", datadump)
+
+	print(shapedata)
+
+if __name__ == '__main__':
+	main()
