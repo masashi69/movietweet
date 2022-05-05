9589faaef547496951a7829dacd6e1750d4075f3
Masashi Suzuki <smasa619@gmail.com>
2022-05-05 18:37:39 +0900

    Added the function to specify the released year

--- getinfo.py
+++ getinfo.py
@@ -3,15 +3,22 @@ import sys
 import json
 import configure
 import re
+import argparse
 
 KEY = configure.APIKEY
 url = 'http://www.omdbapi.com/'
-gettitle = '+'.join(sys.argv[1:])
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--title', nargs='+', help='Input movie title', required=True)
+parser.add_argument('--year',type=int, help='Specify released year')
+args = parser.parse_args()
+
+gettitle = '+'.join(args.title)
 getinfo = ''.join([url, '?apikey=', KEY, '&t=', gettitle])
 
-def main():
+def getmovieinfo(infos=getinfo):
 
-	movieinfo = requests.get(getinfo)
+	movieinfo = requests.get(infos)
 	dataload = json.loads(movieinfo.text)
 
 	datadump = json.dumps(dataload, indent=0)
@@ -20,6 +27,14 @@ def main():
 	shapedata = re.sub(',\n', "\n", shapedata)
 
 	print(shapedata)
+
+def main():
+
+	if args.year:
+		getinfo_y = ''.join([url, '?apikey=', KEY, '&t=', gettitle, '&y={}'.format(args.year)])
+		getmovieinfo(getinfo_y)
+	else:
+		getmovieinfo()
 
 if __name__ == '__main__':
 	main()
