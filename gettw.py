3e5c3686414c74ef641661ce2f69bab2ad4ed3d3
Masashi Suzuki <smasa619@gmail.com>
2022-05-09 22:00:19 +0900

    Created the function for find posted title

--- gettw.py
+++ gettw.py
@@ -10,20 +10,23 @@ args = parser.parse_args()
 
 API = TWauth()
 
+def find_title():
+    Posted = info.created_at + datetime.timedelta(hours=9)
+    Text = info.text[info.text.find('「')+1:info.text.find('」')]
+    return Posted, Text
+
 for i in range(1, 5):
     for info in API.user_timeline(count=200, page=i):
         if 'tweet movieinfo' in info.source:
             if args.year:
-                Posted = info.created_at + datetime.timedelta(hours=9)
-                Text = info.text[info.text.find('「')+1:info.text.find('」')]
+                post, text = find_title()
                 if info.created_at.year == args.year:
-                    print(Posted, Text)
+                    print(post, text)
                 else:
                     pass
             else:
-                Posted = info.created_at + datetime.timedelta(hours=9)
-                Text = info.text[info.text.find('「')+1:info.text.find('」')]
-                print(Posted, Text)
+                post, text = find_title()
+                print(post, text)
         else:
             pass
 
